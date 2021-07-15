958bc25cfeec469b695057ecaf7f6d25eec10919
John Hurt <rita.uy56@gmail.com>
2021-07-15 20:07:10 +1000

    Add project files.

---
+++ Models/SquashMallows.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace FermiParadox2.Models
+{
+    public class SquashMallows
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public DateTime Squishdate { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
